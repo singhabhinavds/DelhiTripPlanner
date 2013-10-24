e6c05d860518e0d0d2b9d6b62db7419cc3fcefd0
Singh Abhinav D S <singhabhinavds@gmail.com>
2013-10-24 07:05:15 +0530

    login form beautification
    Added CSS for login form

--- login/login_form.php
+++ login/login_form.php
@@ -1,6 +1,26 @@
-<form action="process_login.php" method="POST">
-Email : <input type="text" name="user_email">
-Password : <input type="password" name="user_password">
-<input type='submit'>
-</form>
-
+<?php include_once '../config.php';
+ session_start();
+ ?>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel='stylesheet' type='text/css' href='../<?php echo $css_folder; ?>/login.css' />
+    </head>
+<div class ='container'>
+    <form action="process_login.php" method="POST">
+        <ul>
+            <li>
+                Email : <input type="text" name="user_email" id ='email'>
+            </li>
+            <li>
+                Password : <input type="password" name="user_password" id='password'>
+            </li>
+            <li>
+                <input type='submit' name='Login' value='Login' title='Login' id='submit'>
+            </li>
+        </ul>
+    </form>
+    <form action='../signup/signup_form.php' method='GET'>
+        <input type='submit' name='SIGNUP' value='SIGNUP' id='signup' title='signup' />
+    </form>
+</div>
